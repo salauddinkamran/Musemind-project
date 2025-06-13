4882ebed9479bf641bb12d5425bd9c42107863c4
kamran <mma744237@gmail.com>
2025-06-14 01:38:22 +0600

    Responsive Done

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+let menu = document.getElementById("menu");
+let menuBtn = document.getElementById("menu-btn");
+menuBtn.addEventListener("click", function() {
+  menu.classList.toggle("hidden")
+})
+
+
 // var swiper = new Swiper(".mySwiper", {
 //   effect: "cards",
 //   grabCursor: true,
@@ -17,10 +24,60 @@ function toggleAccordion(button) {
   }
 }
 
+var swiper = new Swiper(".myBrand", {
+  grabCursor: true,
+  loop: true,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    "@0.00": {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    "@0.75": {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    "@1.00": {
+      slidesPerView: 5,
+      spaceBetween: 40,
+    },
+    "@1.50": {
+      slidesPerView: 7,
+      spaceBetween: 50,
+    },
+  },
+});
+
+
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 4,
   spaceBetween: 30,
   freeMode: true,
   grabCursor: true,
   loop: true,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    "@0.00": {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    "@0.75": {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    "@1.00": {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    "@1.50": {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+  },
 });
